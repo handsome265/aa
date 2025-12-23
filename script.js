2f1e6b254e7ee7b47ffd1f7c29d87b96efdbee8c
handsome265 <william.linliyang@gmail.com>
2025-12-23 20:42:14 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+const slides = document.querySelectorAll(".slide");
+
+const audioFiles = [
+  "audio/part1.mp3",
+  "audio/part2.mp3",
+  "audio/part3.mp3",
+  "audio/part4.mp3",
+  "audio/part5.mp3"
+];
+
+let index = 0;
+const audio = new Audio();
+
+function playSlide(i) {
+  slides.forEach(slide => slide.classList.remove("active"));
+  slides[i].classList.add("active");
+
+  audio.src = audioFiles[i];
+  audio.play();
+
+  audio.onended = () => {
+    if (i + 1 < slides.length) {
+      playSlide(i + 1);
+    }
+  };
+}
+
+playSlide(0);
 
